08c396f55350b243c1ed94c6b55258317926659a
Roman Zabicki <rfzabick@savoy.local>
2010-11-13 11:52:27 -0600

    fixing the show link

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -24,31 +24,44 @@ class PollsController < ApplicationController
 		end
 	end
 
+
+
 	# GET /polls
 	# GET /polls.xml
 	def index
+    prepare_for_application_html
+    
 		max_id = cookies[:max_id]
 		max_id = max_id.nil? ? 0 : Integer(max_id)
 
 		@answer_me = Poll.where("id > ?", max_id).order("id asc").first
 		if @answer_me 			
-			@category = Category.where("id = ?", @answer_me.category_id).first
+			@category = Category.find @answer_me.category_id
 		end
 
-		@poll = Poll.new
-		@categories = Category.find :all
-		
 		respond_to do |format|
 			format.html # index.html.erb
 		end
 	end
 
+  # GET /polls/1
+  # GET /polls/1.xml
+  def show
+    prepare_for_application_html
+    @poll = Poll.find params[:id]
+    @category = Category.find @poll.category_id
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.xml { render :xml => @poll }
+    end
+  end
+  
   def already_voted
+    prepare_for_application_html
     max_id = cookies[:max_id]
     max_id = max_id.nil? ? 0 : Integer(max_id)
     @polls = Poll.where("id <= ?", max_id)
-    @poll = Poll.new
-    @categories = Category.find :all
   end
 
 	# GET /polls/admin
@@ -64,17 +77,16 @@ class PollsController < ApplicationController
 		end
 	end
 
-	# GET /polls/new
-	# GET /polls/new.xml
-	def new
-		@poll = Poll.new
-		@categories = Category.find :all
-
-		respond_to do |format|
-			format.html # new.html.erb
-			format.xml { render :xml => @poll }
-		end
-	end
+#	# GET /polls/new
+#	# GET /polls/new.xml
+#	def new
+#    prepare_for_application_html
+#
+#		respond_to do |format|
+#			format.html # new.html.erb
+#			format.xml { render :xml => @poll }
+#		end
+#	end
 
 	# GET /polls/1/edit
 	def edit
@@ -107,25 +119,21 @@ class PollsController < ApplicationController
 		flash[:error] = "Nice try. No updating allowed."
 	end
 
-	# DELETE /polls/1
-	# DELETE /polls/1.xml
-	def destroy
-		@poll = Poll.find(params[:id])
-		@poll.destroy
+#	# DELETE /polls/1
+#	# DELETE /polls/1.xml
+#	def destroy
+#		@poll = Poll.find(params[:id])
+#		@poll.destroy
+#
+#		respond_to do |format|
+#			format.html { redirect_to(polls_url) }
+#			format.xml { head :ok }
+#		end
+#	end
 
-		respond_to do |format|
-			format.html { redirect_to(polls_url) }
-			format.xml { head :ok }
-		end
-	end
-
-	def upload_file
-		if(params[:upload])
-			render :text => "File has been uploaded successfully"
-		else
-			respond_to do |format|
-				format.html # index.html.erb
-			end
-		end
-	end
+  private
+  def prepare_for_application_html
+    @poll = Poll.new
+    @categories = Category.all    
+  end
 end
