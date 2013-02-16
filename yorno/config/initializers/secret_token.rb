ca1168e7d65097d61b7484aee9f017a496ddf495
JJide <o_jaji@yahoo.com>
2013-02-16 10:24:01 -0600

    Initial Commit

---
+++ yorno/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Yorno::Application.config.secret_token = '3a26a279c76f284a2954dbbe09b9649e22f97b6ed59e5554baff53fc66d894165a894e8a7d135e3a38006ee13690ab869f7438cf96d849e6b9770a4abae7355b'
